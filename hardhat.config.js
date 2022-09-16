282495e381237cce57b812250332d0f5f06f46b9
aakrut <aakrutdabhi@gmail.com>
2022-09-16 16:04:00 +0530

    added auth-page

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: process.env.URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  path: {
+    artifacts: "./artifacts",
+  },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,68 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Dashboard from "./dashboard";
 
 export default function Home() {
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+
+  const router = useRouter();
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Please Install Metamask");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setIsWalletConnected(true);
+      localStorage.setItem("walletAddress", accounts[0]);
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div >
-      
+    <div>
+      <Head>
+        <title>YouTube</title>
+        <link rel="icon" href="/youtube.png" />
+      </Head>
+      {isWalletConnected && localStorage.getItem("walletAddress") ? (
+        <Dashboard />
+      ) : (
+        <div className="flex h-screen items-center justify-center flex-col gap-5 font-body ">
+          <img
+            src="/youtube_main.png"
+            alt="logo"
+            className="h-[160px] animate-bounce"
+          />
+          <h1 className="text-7xl font-semibold ">
+            Web 3.0{" "}
+            <span className="semibold text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-rose-600">
+              {" "}
+              YouTube
+            </span>
+          </h1>
+          <p className="text-neutral-400">
+            Web 3.0 YouTube made with Next Js, IPFS, The Graph Protocol and with
+            Tailwind CSS
+          </p>
+          <button
+            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 
+            dark:focus:ring-blue-800 font-medium rounded-full text-sm px-8 py-5 text-center mr-2 mb-2 transition-all ease-in-out delay-150 duration-150
+            hover:translate-y-1 text-1xl"
+            onClick={connectWallet}
+          >
+            Connect Wallet
+          </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
