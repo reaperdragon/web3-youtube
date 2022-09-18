ac04c30972b707e47de9908fbf267b6306a52ca5
aakrut <aakrutdabhi@gmail.com>
2022-09-18 12:13:22 +0530

    added component & video page

---
+++ components/VideoCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useRouter } from "next/router";
+
+const ipfsURI = "https://ipfs.io/ipfs/";
+
+const VideoCard = ({ data }) => {
+    const router = useRouter();
+  return (
+    <div
+      key={data.id}
+      className="w-full  h-max sm:h-[25%] font-body mt-2 cursor-pointer"
+      onClick={() => router.push(`/video/${data.id}`)}
+    >
+      <img
+        src={ipfsURI + data.videothumbhash}
+        alt={data.title}
+        className="rounded-lg h-full w-full"
+      />
+      <h3 className="text-lg font-semibold">{data.title}</h3>
+    </div>
+  );
+}
+
+export default VideoCard
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,8 +1,7 @@
 import { gql, useApolloClient, useQuery } from "@apollo/client";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import { Header } from "../components";
+import { Header, VideoCard } from "../components";
 
 const FETCH_VIDEOS = gql`
   query videos($orderBy: String!, $orderDirection: String!) {
@@ -21,14 +20,14 @@ const FETCH_VIDEOS = gql`
   }
 `;
 
-const ipfsURI = "https://ipfs.io/ipfs/";
+
 
 const Dashboard = () => {
   const [videos, setVideos] = useState([]);
 
   const clientApollo = useApolloClient();
 
-  const router = useRouter();
+
 
   // Main Approach ---------------------
 
@@ -77,19 +76,7 @@ const Dashboard = () => {
       <div className="grid grid-cols-5 gap-6 h-max md:grid-cols-2 sm:grid-cols-1 px-[28px] sm:px-1 sm:gap-1 md:gap-y-1 max-w-[1440px] my-0 mx-auto">
         {videos &&
           videos?.videos?.map((data) => (
-            <div
-              key={data.id}
-              className="w-full  h-max sm:h-[25%] font-body mt-2 cursor-pointer"
-              onClick={() => router.push(`/video/${data.id}`)}
-            >
-              <img
-                src={ipfsURI + data.videothumbhash}
-                alt={data.title}
-                className="rounded-lg"
-              />
-              <h3 className="text-lg font-semibold">{data.title}</h3>
-              <p className="text-slate-400"> {data.description}</p>
-            </div>
+            <VideoCard key={data.id} data={data} />
           ))}
       </div>
     </div>
