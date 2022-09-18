85778cc44f2f8dc5b5896c2367439c1009b78068
aakrut <aakrutdabhi@gmail.com>
2022-09-18 20:03:22 +0530

    added search functionality

--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,85 @@
-import React from 'react'
+import { gql, useApolloClient, useLazyQuery } from "@apollo/client";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Header, VideoCard } from "../components";
 
 const Search = () => {
-  return (
-      <div>
-          
-    </div>
-  )
-}
+  const [searchFilter, setSearchFilter] = useState("");
 
-export default Search
+  const [videos, setVideos] = useState([]);
+
+  const clientApollo = useApolloClient();
+
+  const SEARCH_VIDEO = useMemo(
+    () => gql`
+      query videos(
+        $orderBy: Video_orderBy
+        $orderDirection: OrderDirection
+        $where: Video_filter
+      ) {
+        videos(
+          orderBy: $orderBy
+          orderDirection: $orderDirection
+          where: $where
+        ) {
+          id
+          videothumbhash
+          videohash
+          title
+        }
+      }
+    `,
+    []
+  );
+
+  const getVideos = useCallback(async () => {
+    clientApollo
+      .query({
+        query: SEARCH_VIDEO,
+        variables: {
+          orderBy: "createdAt",
+          orderDirection: "desc",
+          where: {
+            ...(searchFilter && {
+              title_contains_nocase: searchFilter,
+            }),
+          },
+        },
+        fetchPolicy: "network-only",
+      })
+      .then(({ data }) => {
+        console.log(data);
+        setVideos(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [SEARCH_VIDEO, clientApollo, searchFilter]);
+
+  useEffect(() => {
+    getVideos();
+  }, [searchFilter, getVideos]);
+
+  return (
+    <div>
+      <Header />
+      <input
+        placeholder="Search Video"
+        value={searchFilter}
+        onChange={(e) => setSearchFilter(e.target.value)}
+      />
+
+      {searchFilter ? (
+        <div className="grid grid-cols-5 gap-6 h-max md:grid-cols-2 sm:grid-cols-1 px-[28px] sm:px-1 sm:gap-1 md:gap-y-1 max-w-[1440px] my-0 mx-auto">
+          {videos &&
+            videos?.videos?.map((data) => (
+              <VideoCard key={data.id} data={data} />
+            ))}
+        </div>
+      ) : (
+        <h1>Search Video</h1>
+      )}
+    </div>
+  );
+};
+
+export default Search;
