f18e46735ec0efc7f84e528924944cf33d603e5a
aakrut <aakrutdabhi@gmail.com>
2022-09-18 15:20:33 +0530

    fixed styling

--- components/VideoCard.js
+++ components/VideoCard.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 import { useRouter } from "next/router";
 
 const ipfsURI = "https://ipfs.io/ipfs/";
 
 const VideoCard = ({ data }) => {
-    const router = useRouter();
+  const router = useRouter();
   return (
     <div
       key={data.id}
-      className="w-full  h-max sm:h-[25%] font-body mt-2 cursor-pointer"
+      className="w-full h-max sm:h-[25%] font-body mt-2 cursor-pointer"
       onClick={() => router.push(`/video/${data.id}`)}
     >
-      <img
-        src={ipfsURI + data.videothumbhash}
-        alt={data.title}
-        className="rounded-lg h-full w-full"
-      />
+      <div className="h-[186px] w-full">
+        <img
+          src={ipfsURI + data.videothumbhash}
+          alt={data.title}
+          className="rounded-lg h-full w-full"
+        />
+      </div>
       <h3 className="text-lg font-semibold">{data.title}</h3>
     </div>
   );
-}
+};
 
-export default VideoCard
+export default VideoCard;
